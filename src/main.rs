da6dcd38783e0a99588a01d7b02c110930b453c7
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-04-25 00:45:16 +1000

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use spam::{csr_matrix::CsrMatrix, dok_matrix::parse_matrix_market};
-use std::fs::read_to_string;
+use std::{env::args, fs::read_to_string};
 
 fn main() {
-    let f = "engine";
+    let path = args().nth(1).unwrap();
     let m1 = CsrMatrix::from(
-        parse_matrix_market::<i32>(&read_to_string(format!("matrices/{}.mtx", f)).unwrap())
+        parse_matrix_market::<i32>(&read_to_string(path).unwrap())
             .unwrap()
             .1,
     );
