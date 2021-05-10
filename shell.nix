18dee7f3a4883d20119183bb3c3f78773e904667
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-05-10 19:23:22 +1000

    revert flakes

--- shell.nix
+++ shell.nix
@@ -1,13 +1,41 @@
-(import
-  (
-    let
-      lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-    in
-    fetchTarball {
-      url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-      sha256 = lock.nodes.flake-compat.locked.narHash;
-    }
-  )
-  {
-    src = ./.;
-  }).shellNix
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell
+{
+  buildInputs = (with pkgs; [ fontconfig.dev cargo-fuzz arrayfire hyperfine ]) ++ (with pkgs; [
+    git
+    gitRepo
+    gnupg
+    autoconf
+    curl
+    procps
+    gnumake
+    utillinux
+    m4
+    gperf
+    unzip
+    cudatoolkit
+    linuxPackages.nvidia_x11
+    libGLU
+    libGL
+    xorg.libXi
+    xorg.libXmu
+    freeglut
+    xorg.libXext
+    xorg.libX11
+    xorg.libXv
+    xorg.libXrandr
+    zlib
+    ncurses5
+    stdenv.cc
+    binutils
+  ]);
+  nativeBuildInputs = with pkgs; [ cmake pkg-config ];
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  shellHook = ''
+    export CUDA_PATH=${pkgs.cudatoolkit}
+    # export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+    export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+    export EXTRA_CCFLAGS="-I/usr/include"
+  '';
+}
+            
