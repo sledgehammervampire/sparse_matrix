83c77aa002d0fb3a43e3361814ae05d375b086ec
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-05-17 02:32:48 +1000

    use ahash instead of fxhash

--- src/csr_matrix.rs
+++ src/csr_matrix.rs
@@ -1,7 +1,7 @@
+use ahash::AHashMap;
 use itertools::{iproduct, Itertools};
 use num::Num;
-use rayon::{iter::ParallelIterator, prelude::*};
-use rustc_hash::FxHashMap;
+use rayon::prelude::*;
 use std::{
     borrow::Cow,
     collections::BTreeMap,
@@ -275,7 +275,7 @@ impl<T: Num + Clone + Send + Sync> Mul for &CsrMatrix<T> {
             .par_iter()
             .zip(self.ridx.par_iter().skip(1))
             .map(|(&a, &b)| {
-                let mut row = FxHashMap::default();
+                let mut row = AHashMap::new();
 
                 self.cidx[a..b]
                     .iter()
--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ fn main() -> anyhow::Result<()> {
 fn foo<T: Num + Clone + Send + Sync + Debug>(m1: DokMatrix<T>, f: &OsStr) {
     let m1 = CsrMatrix::from(m1);
     if let Some(&len) = m1.row_nnz_freq().keys().last() {
-        if len > 10000 {
+        if len > 15000 {
             return;
         }
     }
