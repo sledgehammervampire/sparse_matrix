f9516db9c48c8ef8638d2ce7e4ec5d14bf97249b
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-06-11 20:12:35 +1000

    refactor benches

--- benches/bench_mul_heap.rs
+++ benches/bench_mul_heap.rs
@@ -10,23 +10,17 @@ use spam::{
 use walkdir::WalkDir;
 
 pub fn bench_mul_heap(c: &mut Criterion) {
-    fn foo<T: Clone + NumAssign + Send + Sync>(c: &mut Criterion, f: &OsStr, m1: DokMatrix<T>) {
-        let m1 = CsrMatrix::from(m1);
-        if let Some(&len) = m1.row_nnz_freq().keys().last() {
-            if len > 10000 {
-                return;
-            }
-        }
-        let m2 = m1.clone();
+    fn foo<T: Clone + NumAssign + Send + Sync>(c: &mut Criterion, f: &OsStr, m: DokMatrix<T>) {
+        let m = CsrMatrix::from(m);
         c.bench_function(
             &format!(
                 "bench mul_heap {:?} ({}x{}, {} nonzero entries)",
                 f,
-                m1.rows(),
-                m1.cols(),
-                m1.nnz()
+                m.rows(),
+                m.cols(),
+                m.nnz()
             ),
-            |b| b.iter(|| m1.mul_heap(&m2)),
+            |b| b.iter(|| m.mul_heap(&m)),
         );
     }
 
@@ -39,14 +33,14 @@ pub fn bench_mul_heap(c: &mut Criterion) {
                     .unwrap()
                     .1
                 {
-                    MatrixType::Integer(m1) => {
-                        foo(c, f, m1);
+                    MatrixType::Integer(m) => {
+                        foo(c, f, m);
                     }
-                    MatrixType::Real(m1) => {
-                        foo(c, f, m1);
+                    MatrixType::Real(m) => {
+                        foo(c, f, m);
                     }
-                    MatrixType::Complex(m1) => {
-                        foo(c, f, m1);
+                    MatrixType::Complex(m) => {
+                        foo(c, f, m);
                     }
                 }
             }
