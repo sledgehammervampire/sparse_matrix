1fb2acf734e0eec88bc00a931b63209661ab92a8
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-06-21 02:31:56 +1000

    remove stale fuzz targets

--- spam/fuzz/fuzz_targets/arb_csr_invariants.rs
+++
@@ -1,8 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use spam::{csr_matrix::CsrMatrix, dok_matrix::DokMatrix};
-
-fuzz_target!(|m: DokMatrix<i8>| {
-    let m = CsrMatrix::from(m);
-    assert!(m.invariants());
-});
--- spam/fuzz/fuzz_targets/arb_dok_invariants.rs
+++
@@ -1,7 +0,0 @@
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use spam::dok_matrix::DokMatrix;
-
-fuzz_target!(|m: DokMatrix<i8>| {
-    assert!(m.invariants());
-});
