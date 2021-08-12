3acd45815ecc32c395d886416dedd3b9836e48b1
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-08-13 02:49:14 +1000

    refactor main to allow running mul_hash and mkl_spmm

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use cap_std::fs::Dir;
+use num::{traits::NumAssign, Num};
 use open_ambient::open_ambient_dir;
 use spam::{
-    csr_matrix::CsrMatrix,
-    dok_matrix::{parse_matrix_market, MatrixType},
+    csr_matrix::{
+        mkl::{MklCsrMatrix, RustMklSparseMatrix},
+        CsrMatrix,
+    },
+    dok_matrix::{parse_matrix_market, DokMatrix, MatrixType},
 };
-use std::io::Read;
+use std::{convert::TryFrom, io::Read};
+
+const ITERS: usize = 100;
 
 fn main() -> anyhow::Result<()> {
     let dir = open_ambient_dir!("matrices")?;
@@ -18,25 +24,37 @@ fn bench_mul(dir: Dir) -> anyhow::Result<()> {
         let mut input = String::new();
         entry.open()?.read_to_string(&mut input)?;
         match parse_matrix_market::<i64, f64>(&input).unwrap() {
-            MatrixType::Integer(m) => {
-                let m = CsrMatrix::from(m);
-                for _ in 0..100 {
-                    let _: CsrMatrix<_, false> = m.mul_hash(&m);
-                }
-            }
+            MatrixType::Integer(_) => {}
             MatrixType::Real(m) => {
-                let m = CsrMatrix::from(m);
-                for _ in 0..100 {
-                    let _: CsrMatrix<_, false> = m.mul_hash(&m);
-                }
+                mul_hash(m);
             }
             MatrixType::Complex(m) => {
-                let m = CsrMatrix::from(m);
-                for _ in 0..100 {
-                    let _: CsrMatrix<_, false> = m.mul_hash(&m);
-                }
+                mul_hash(m);
             }
         }
     }
     Ok(())
+}
+
+pub fn mkl_spmm<T, U>(m: DokMatrix<T>)
+where
+    T: Num,
+    MklCsrMatrix<U, true>: TryFrom<CsrMatrix<T, true>>,
+    for<'a> RustMklSparseMatrix<'a, U, true>: TryFrom<&'a mut MklCsrMatrix<U, true>>,
+{
+    let m = CsrMatrix::from(m);
+    if let Ok(mut m) = MklCsrMatrix::try_from(m) {
+        if let Ok(m) = RustMklSparseMatrix::try_from(&mut m) {
+            for _ in 0..ITERS {
+                let _ = &m * &m;
+            }
+        }
+    }
+}
+
+pub fn mul_hash<T: NumAssign + Copy + Send + Sync>(m: DokMatrix<T>) {
+    let m = CsrMatrix::from(m);
+    for _ in 0..ITERS {
+        let _: CsrMatrix<_, false> = m.mul_hash(&m);
+    }
 }
