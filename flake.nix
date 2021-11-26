8d1abd7ab7fb529e8f65eb71420b6365d76e46f7
1000teslas <47207223+1000teslas@users.noreply.github.com>
2021-11-26 21:57:59 +1100

    update deps

--- flake.nix
+++ flake.nix
@@ -9,59 +9,52 @@
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay }: flake-utils.lib.eachDefaultSystem (
     system:
-      let
-        overlays = [
-          (import rust-overlay)
-        ];
-        pkgs = import nixpkgs { inherit system overlays; config.allowUnfree = true; };
-      in
-        {
-          devShell =
-            with pkgs; mkShell
-              {
-                buildInputs =
-                  let
-                    # rust = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-                    rust = rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override { extensions = [ "rust-src" "miri" "llvm-tools-preview" ]; });
-                  in
-                    [
-                      rust
-                      bashInteractive
-                      cargo-edit
-                      cargo-fuzz
-                      cargo-binutils
-                      cargo-flamegraph
-                      hotspot
-                      cargo-criterion
-                      pkg-config
-                      mkl
-                      coz
-                      rust-bindgen
-                      rr
-                      valgrind
-                      cargo-expand
-                      linuxPackages.perf
-                      cargo-geiger
-                      cargo-criterion
-                      cargo-bloat
-                      delta
-                      (wrapBintoolsWith { bintools = llvmPackages_latest.bintools-unwrapped; })
-                      cargo-audit
-                      cargo-supply-chain
-                    ] ++ (
-                      with llvmPackages_latest; [
-                        clang-unwrapped.lib
-                        libllvm
-                      ]
-                    );
-                MKLROOT = "${mkl}";
-                LIBCLANG_PATH = "${llvmPackages_latest.clang-unwrapped.lib}/lib";
-                RUSTFLAGS = "-Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,--no-as-needed -Clink-arg=-Wl,-lmkl_intel_ilp64 -Clink-arg=-Wl,-lmkl_intel_thread -Clink-arg=-Wl,-lmkl_core -Clink-arg=-Wl,-liomp5 -Clink-arg=-Wl,--as-needed";
-                # RUSTC_LOG = "rustc_codegen_ssa::back::link=debug";
-                # RUSTFLAGS = "-C link-arg=-Wl,--verbose";
-                # RUSTFLAGS = "-Z gcc-ld=lld";
-                # LD_DEBUG = "all";
-              };
-        }
+    let
+      overlays = [
+        (import rust-overlay)
+      ];
+      pkgs = import nixpkgs { inherit system overlays; config.allowUnfree = true; };
+    in
+    {
+      devShell =
+        with pkgs; mkShell
+          {
+            buildInputs =
+              let
+                # rust = rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+                rust = rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override { extensions = [ "rust-src" "miri" "llvm-tools-preview" ]; });
+              in
+              [
+                rust
+                bashInteractive
+                cargo-edit
+                cargo-fuzz
+                cargo-binutils
+                cargo-flamegraph
+                hotspot
+                cargo-criterion
+                pkg-config
+                mkl
+                linuxPackages.perf
+                cargo-criterion
+                cargo-bloat
+                (wrapBintoolsWith { bintools = llvmPackages_latest.bintools-unwrapped; })
+                cargo-audit
+                cargo-supply-chain
+              ] ++ (
+                with llvmPackages_latest; [
+                  clang-unwrapped.lib
+                  libllvm
+                ]
+              );
+            MKLROOT = "${mkl}";
+            LIBCLANG_PATH = "${llvmPackages_latest.clang-unwrapped.lib}/lib";
+            RUSTFLAGS = "-Clink-arg=-fuse-ld=lld -Clink-arg=-Wl,--no-as-needed -Clink-arg=-Wl,-lmkl_intel_ilp64 -Clink-arg=-Wl,-lmkl_intel_thread -Clink-arg=-Wl,-lmkl_core -Clink-arg=-Wl,-liomp5 -Clink-arg=-Wl,--as-needed";
+            # RUSTC_LOG = "rustc_codegen_ssa::back::link=debug";
+            # RUSTFLAGS = "-C link-arg=-Wl,--verbose";
+            # RUSTFLAGS = "-Z gcc-ld=lld";
+            # LD_DEBUG = "all";
+          };
+    }
   );
 }
